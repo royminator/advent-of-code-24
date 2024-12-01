2fe5813327f11f2ad3c330e265e3cd82be865b29
Roy Angelsen <roy.angelsen@gmail.com>
2024-12-01 09:21:41 +0100

    Initial commit

---
+++ lib/advent_of_code24.ex
@@ -0,0 +1,18 @@
+defmodule AdventOfCode24 do
+  @moduledoc """
+  Documentation for `AdventOfCode24`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventOfCode24.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/advent_of_code24_test.exs
@@ -0,0 +1,8 @@
+defmodule AdventOfCode24Test do
+  use ExUnit.Case
+  doctest AdventOfCode24
+
+  test "greets the world" do
+    assert AdventOfCode24.hello() == :world
+  end
+end
