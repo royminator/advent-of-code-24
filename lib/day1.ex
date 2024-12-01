b34aa956473f6215eda2956ac58deaa8241e39ac
Roy Angelsen <roy.angelsen@gmail.com>
2024-12-01 21:59:56 +0100

    feat: day 1, part 2

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,8 +1,10 @@
 defmodule AdventOfCode24.Day1 do
   def main do
-    read_input_to_lists("res/input/day1.txt")
-    |> calculate_distance()
-    |> Enum.sum()
+    input = read_input_to_lists("res/input/day1.txt")
+
+    {input
+     |> calculate_distance()
+     |> Enum.sum(), input |> similarity_score}
   end
 
   def read_input_to_lists(path) do
@@ -13,12 +15,14 @@ defmodule AdventOfCode24.Day1 do
   end
 
   def parse_input(content) do
-    {list1, list2} =
-      String.split(content, "\r\n", trim: true)
-      |> Enum.map(&line_to_list_of_ints/1)
-      |> Enum.map(&List.to_tuple/1)
-      |> Enum.unzip()
+    String.split(content, "\r\n", trim: true)
+    |> Enum.map(&line_to_list_of_ints/1)
+    |> Enum.map(&List.to_tuple/1)
+    |> Enum.unzip()
+    |> sort_both
+  end
 
+  def sort_both({list1, list2}) do
     {Enum.sort(list1), Enum.sort(list2)}
   end
 
@@ -30,5 +34,16 @@ defmodule AdventOfCode24.Day1 do
 
   def calculate_distance({list1, list2}) do
     Enum.zip_with(list1, list2, fn a, b -> abs(a - b) end)
+  end
+
+  def similarity_score({list1, list2}) do
+    list1
+    |> Enum.map(&similarity_for(&1, list2))
+    |> Enum.sum()
+  end
+
+  def similarity_for(number, list) do
+    Enum.count(list, fn x -> x == number end)
+    |> then(fn n -> n * number end)
   end
 end
