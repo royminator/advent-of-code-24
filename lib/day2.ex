1fb06662d0407d0371a78475039a1f7bc8f0d214
Roy Angelsen <roy.angelsen@gmail.com>
2024-12-13 18:53:42 +0100

    refactor: day2

--- lib/day2.ex
+++ lib/day2.ex
@@ -5,12 +5,13 @@ defmodule AdventOfCode24.Day2 do
     defstruct prev: 0,
               dir: :undefined,
               is_initial: true,
-              is_safe: false
+              is_safe: false,
+              n_fails: 0
   end
 
   def main do
     input = read_and_parse_input("res/input/day2.txt", &parse_input/1)
-    {input |> check_reports}
+    {input |> part1}
   end
 
   def parse_input(content) do
@@ -18,26 +19,21 @@ defmodule AdventOfCode24.Day2 do
     |> Enum.map(&line_to_list_of_ints/1)
   end
 
-  def check_reports(reports) do
-    Enum.reduce(reports, 0, &count_safe_reports/2)
+  def part1(reports) do
+    reports
+    |> Enum.map(&check_safety/1)
+    |> Enum.filter(fn safety -> safety.is_safe end)
+    |> Enum.count()
   end
 
-  def count_safe_reports(report, acc) do
-    case is_safe(report) do
-      true -> acc + 1
-      _ -> acc
-    end
-  end
-
-  def is_safe(report) do
-    safety = Enum.reduce_while(report, %ReportSafety{}, &process_next/2)
-    safety.is_safe
+  def check_safety(report) do
+    Enum.reduce_while(report, %ReportSafety{}, &process_next/2)
   end
 
   def process_next(val, acc) do
     case acc.is_initial do
-      true -> {:cont, %ReportSafety{prev: val, dir: :undefined, is_safe: true, is_initial: false}}
-      _ -> is_report_still_safe(val, acc)
+      true -> {:cont, %{prev: val, dir: :undefined, is_safe: true, is_initial: false}}
+      false -> is_report_still_safe(val, acc)
     end
   end
 
@@ -48,8 +44,8 @@ defmodule AdventOfCode24.Day2 do
       true -> {:halt, %ReportSafety{is_safe: false}}
       false ->
         case is_valid_diff(diff, dir) do
-          true -> {:cont, %ReportSafety{prev: val, dir: dir, is_safe: true, is_initial: false}}
-          false -> {:halt, %ReportSafety{is_safe: false, is_initial: false}}
+          true -> {:cont, %{prev: val, dir: dir, is_safe: true, is_initial: false}}
+          false -> {:halt, %{is_safe: false, is_initial: false}}
         end
     end
   end
@@ -62,13 +58,7 @@ defmodule AdventOfCode24.Day2 do
   def get_dir(diff) when diff > 0, do: :decreasing
   def get_dir(diff) when diff == 0, do: :undefined
 
-  def is_valid_diff(diff, :decreasing) do
-    diff <= 3 && diff > 0
-  end
-
-  def is_valid_diff(diff, :increasing) do
-    diff < 0 && diff >= -3
-  end
-
-  def is_valid_diff(_diff: :undefined), do: false
+  def is_valid_diff(diff, :decreasing), do: diff <= 3 && diff > 0
+  def is_valid_diff(diff, :increasing), do: diff < 0 && diff >= -3
+  def is_valid_diff(_: :undefined), do: false
 end
